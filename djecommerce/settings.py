1d6372303c09a68418b25733cfb50424f9eb9401
Yeswanthteja1010 <111886327+Yeswanthteja1010@users.noreply.github.com>
2024-06-26 18:51:07 +0530

    Update settings.py

--- djecommerce/settings.py
+++ djecommerce/settings.py
@@ -96,9 +96,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build ','static_in_env')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build ','static_in_env')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
+
+
 
 
 # Auth
