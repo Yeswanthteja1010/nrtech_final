41511e05c45a128b0a7392146b3f8b0b875b1def
Yeswanthteja1010 <111886327+Yeswanthteja1010@users.noreply.github.com>
2024-06-26 19:56:22 +0530

    Update settings.py

--- djecommerce/settings.py
+++ djecommerce/settings.py
@@ -11,7 +11,7 @@ environ.Env.read_env()
 SECRET_KEY = config('SECRET_KEY')
 
 #DEBUG = config('DEBUG', default=False, cast=bool)
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
